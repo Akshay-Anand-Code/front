9a1ecd5c037e4d79f32dfaa017074f29f9cec449
Akshay-Anand-Code <akshay1079410@gmail.com>
2024-12-07 18:04:09 +0530

    lfgg

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:3000",
+                target: "https://eliza-starter-2.onrender.com/",
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
             },
